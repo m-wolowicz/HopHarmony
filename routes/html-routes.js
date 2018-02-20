bc9b24dde7d44be947fe0a9bf33229d590dfee5c
Melissa Wolowicz <melissawolowicz@MW-iMac.local>
2018-02-19 19:26:54 -0500

    Working on results page HTML and CSS, not done

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var path = require("path");
 
 /* GET home page. */
 router.get('/', function(req, res) {
 	res.render('index');
+});
+
+/* GET RESULTS PAGE */
+router.get('/results', function(req, res) {
+	res.sendFile(path.join(__dirname, "../public/results.html"));
 });
 
 module.exports = router;
