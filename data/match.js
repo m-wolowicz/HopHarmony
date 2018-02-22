c41dd8d1c4bbfdf55f1208abacca126dafd45d73
Ejike Onwe <ejikeonwe@Ejikes-MacBook-Pro.local>
2018-02-22 03:02:59 -0500

    Partially polpulated the match data

---
+++ data/match.js
@@ -0,0 +1,36 @@
+var bunnies = [
+
+    {
+        name: "Adam",
+        age: "18-22",
+        gender: "male",
+        destination: "ny"
+    }, 
+
+    {
+    
+        name: "Melissa",
+        age: "18-22",
+        gender: "female",
+        destination: "ny"
+    
+    },
+
+    {
+    
+        name: "Ejike",
+        age: "18-22",
+        gender: "male",
+        destination: "ny"
+    
+    },
+
+    {
+        name: "David",
+        age: "18-22",
+        gender: "male",
+        destination: "ny"
+    }
+]
+
+module.exports = bunnies;
