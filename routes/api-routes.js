bfd66e4143d23444b02251d7debc7953a3de557b
Ejike Onwe <ejikeonwe@Ejikes-MacBook-Pro.local>
2018-02-22 10:08:22 -0500

    modfied index.html and files in routes folder

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -5,4 +5,23 @@ module.exports = function (app) {
     app.get("/api/bunnies", function(req, res) {
         res.json(bunnies);
     });
+
+    app.post("/api/bunnies", function (req, res) {
+       
+        var match = {
+            name: "",
+            age: "",
+            matchDifference: 1000,  
+        };
+
+        console.log(req.body);
+
+        // Here we take the the user entry POST and parse it.
+        var userData = req.body;
+         
+
+
+
+    });
+
 }
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -9,14 +9,12 @@ var path = require("path");
 
 /* GET RESULTS PAGE */
 // router.get('/results', function(req, res) {
-// 	console.log("Feed the Bunny button working");
 // 	res.sendFile(path.join(__dirname, "../public/results.html"));
 // });
 
 // module.exports = router;
 module.exports = function (app) {
 	app.get('/results', function (req, res) {
-		console.log("Feed the Bunny button working");
 		res.sendFile(path.join(__dirname, "../public/results.html"));
 	});
 }
